7e3fe9f56de0e1f7353dfedf0b96fbb901698017
Robert Gevorgyan <robert1999.g@gmail.com>
2017-03-18 01:45:36 +0400

    Complete project

--- lib/app.jsx
+++ lib/app.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import countries from '../countries'
 
 class Filter extends React.Component {
   constructor() {
     super();
+    this.state = {query: ''};
+  }
+
+  textChangeHandler = (e) => {
+    const query = e.target.value.toLowerCase();
+    this.setState({ query });
   }
 
   render() {
+    const filteredCountries = countries.map((country, idx) => {
+      const { name : oName, code : oCode } = country;
+      const name = oName.toLowerCase();
+      const code = oCode.toLowerCase();
+      const { query } = this.state;
+      if (name.includes(query) || code.includes(query)) {
+        return <li key={idx}>{oName}</li>
+      }
+    });
     return (
-      <div>
-        Hello world!!!
+      <div className='container'>
+        <h2>Filtered List</h2>
+        <input type='text' placeholder='Filter...'  onChange={this.textChangeHandler} />
+        <div className='list'>
+          {filteredCountries}
+        </div>
       </div>
     );
   }
